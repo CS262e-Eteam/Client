0178ebc642202bc894001a56bab75702ed60d853
Jonathan Opio <jonathanopio@gmail.com>
2019-11-01 13:27:50 -0400

    The drawer menu will filter between books and clothing when each is pressed, and all items shows all. Will work on the background color for the book and clothing pages..

---
+++ lib/pages/filter_book.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:lab03/components/summary_card.dart';
+import 'package:lab03/shared/globals.dart' as globals;
+import 'package:lab03/shared/colors.dart' as colors;
+
+class FilterBook extends StatefulWidget {
+  FilterBook({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  FilterBookState createState() => FilterBookState();
+}
+
+class FilterBookState extends State<FilterBook> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<Widget> summaryCards = [];
+    globals.testItems.forEach((item) {
+      if (item.category == 'Textbook') {
+        summaryCards.add(SummaryCard(item: item));
+      }
+    });
+
+    return CustomScrollView(
+      slivers: <Widget>[
+        const SliverAppBar(
+          backgroundColor: colors.grayBlue,
+          pinned: true,
+          expandedHeight: 250.0,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(
+              globals.appName,
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return Container (
+                height: globals.testItems.length % 2 == 0 ?
+                  (globals.testItems.length ~/ 2).toDouble() * 250 :
+                  (globals.testItems.length ~/ 2).toDouble() * 250 + 250,
+                child: GridView.count(
+                  childAspectRatio: .8,
+                  primary: false,
+                  padding: const EdgeInsets.all(20),
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                  crossAxisCount: 2,
+                  children: summaryCards,
+                )
+              );
+            },
+            childCount: 1,
+          ),
+        ),
+      ],
+    );
+  }
+}
