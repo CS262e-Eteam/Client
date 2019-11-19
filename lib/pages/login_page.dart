768a6ba85c6111591983ea951a5f539c34b78691
Sameer Mall <sameerm@pectechnologies.com>
2019-11-18 19:57:20 -0500

    test for jason

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,15 +5,15 @@ Login Page - allows a user to log in or navigate to sign up page
 import 'package:flutter/material.dart';
 import 'package:lab03/pages/signup_page.dart';
 import 'package:lab03/pages/home_page.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lab03/shared/colors.dart' as colors;
 
 class LoginPage extends StatefulWidget{
   @override
-//	LoginPageState createState() => new LoginPageState();
-  State<StatefulWidget> createState(){
-	return LoginPageState();
-  }
+	LoginPageState createState() => new LoginPageState();
+//  State<StatefulWidget> createState(){
+//	return LoginPageState();
+//  }
 }
 
 class LoginPageState extends State<LoginPage> {
@@ -35,181 +35,183 @@ class LoginPageState extends State<LoginPage> {
 			title: Text("Login"),
 			backgroundColor: colors.grayBlue,
 	  ),
-//		body: Form(
-//			key: _formKey,
-//			child: Column(
-//				children: <Widget>[
-//					TextFormField(
-//						validator: (input) {
-//							if(input.isEmpty) {
-//								return 'Please enter an email';
-//							}
-//							return null;
-//						} ,
-//						onSaved: (input) => _email = input,
-//						decoration: InputDecoration(
-//							labelText: 'Email'
-//						),
-//					),
-//					TextFormField(
-//						validator: (input) {
-//							if(input.length < 6) {
-//								return 'Your password must be atleast 6 characters';
-//							}
-//							return null;
-//						} ,
-//						onSaved: (input) => _password = input,
-//						decoration: InputDecoration(
-//							labelText: 'Password'
-//						),
-//						obscureText: true,
-//					),
-//					RaisedButton(
-//						onPressed: () {} ,
-//						child: Text('Sign in'),
-//					)
-//				],
-//			),
-//		),
-
-		body: ListView(
-			padding: EdgeInsets.all(20),
-			children: <Widget>[
-
-				Text(
-					"Login",
-					style: TextStyle(
-						fontSize: 24,
-						fontWeight: FontWeight.bold,
-					),
-				),
-				Padding(
-					padding: EdgeInsets.only(bottom: 15)
-				),
-				Padding(
-					padding: EdgeInsets.symmetric(horizontal: 10),
-					child: TextFormField(
-						controller: emailLogin,
-						focusNode: emailNode,
-						validator: (value) {
-							if (value.isEmpty) return "Please enter an Email";
+		body: Form(
+			key: _formKey,
+			child: Column(
+				children: <Widget>[
+					TextFormField(
+						validator: (input) {
+							if(input.isEmpty) {
+								return 'Please enter an email';
+							}
 							return null;
-						},
-						keyboardType: TextInputType.text,
-						cursorColor: colors.lightBerry,
+						} ,
+						onSaved: (input) => _email = input,
 						decoration: InputDecoration(
-							labelText: "Email",
-							labelStyle: TextStyle(
-								color: emailNode.hasFocus ? colors.lightBerry : Colors.black
-							),
-							focusedBorder: UnderlineInputBorder(
-								borderSide: BorderSide(color: colors.lightBerry),
-							),
+							labelText: 'Email'
 						),
 					),
-				),
-				Padding(
-					padding: EdgeInsets.symmetric(horizontal: 10),
-					child: TextFormField(
-						controller: passwordLogin,
-						focusNode: passwordNode,
-						validator: (value) {
-							if (value.isEmpty) return "Please enter a Password";
+					TextFormField(
+						validator: (input) {
+							if(input.length < 6) {
+								return 'Your password must be atleast 6 characters';
+							}
 							return null;
-						},
-						keyboardType: TextInputType.text,
-						cursorColor: colors.lightBerry,
+						} ,
+						onSaved: (input) => _password = input,
 						decoration: InputDecoration(
-							labelText: "Password",
-							labelStyle: TextStyle(
-								color: passwordNode.hasFocus ? colors.lightBerry : Colors.black
-							),
-							focusedBorder: UnderlineInputBorder(
-								borderSide: BorderSide(color: colors.lightBerry),
-							),
+							labelText: 'Password'
 						),
+						obscureText: true,
 					),
-				),
+					RaisedButton(
+						onPressed: signIn,
+						child: Text('Sign in'),
+					)
+				],
+			),
+		),
+	);
+
+
+//		body: ListView(
+//			padding: EdgeInsets.all(20),
+//			children: <Widget>[
+//
+//				Text(
+//					"Login",
+//					style: TextStyle(
+//						fontSize: 24,
+//						fontWeight: FontWeight.bold,
+//					),
+//				),
+//				Padding(
+//					padding: EdgeInsets.only(bottom: 15)
+//				),
 //				Padding(
 //					padding: EdgeInsets.symmetric(horizontal: 10),
-//					child: Column(
-//						crossAxisAlignment: CrossAxisAlignment.start,
+//					child: TextFormField(
+//						controller: emailLogin,
+//						focusNode: emailNode,
+//						validator: (value) {
+//							if (value.isEmpty) return "Please enter an Email";
+//							return null;
+//						},
+//						keyboardType: TextInputType.text,
+//						cursorColor: colors.lightBerry,
+//						decoration: InputDecoration(
+//							labelText: "Email",
+//							labelStyle: TextStyle(
+//								color: emailNode.hasFocus ? colors.lightBerry : Colors.black
+//							),
+//							focusedBorder: UnderlineInputBorder(
+//								borderSide: BorderSide(color: colors.lightBerry),
+//							),
+//						),
+//					),
+//				),
+//				Padding(
+//					padding: EdgeInsets.symmetric(horizontal: 10),
+//					child: TextFormField(
+//						controller: passwordLogin,
+//						focusNode: passwordNode,
+//						validator: (value) {
+//							if (value.isEmpty) return "Please enter a Password";
+//							return null;
+//						},
+//						keyboardType: TextInputType.text,
+//						cursorColor: colors.lightBerry,
+//						decoration: InputDecoration(
+//							labelText: "Password",
+//							labelStyle: TextStyle(
+//								color: passwordNode.hasFocus ? colors.lightBerry : Colors.black
+//							),
+//							focusedBorder: UnderlineInputBorder(
+//								borderSide: BorderSide(color: colors.lightBerry),
+//							),
+//						),
+//					),
+//				),
+////				Padding(
+////					padding: EdgeInsets.symmetric(horizontal: 10),
+////					child: Column(
+////						crossAxisAlignment: CrossAxisAlignment.start,
+////						children: <Widget>[
+////							Text(
+////								"Email"
+////							),
+////							TextField(
+////								controller: emailLogin,
+////							),
+////							Padding(
+////								padding: EdgeInsets.only(bottom: 15)
+////							),
+////							Text(
+////								"Password"
+////							),
+////							TextField(
+////								controller: passwordLogin,
+////							),
+////						],
+////					)
+////				),
+//				Padding(
+//					padding: EdgeInsets.only(bottom: 15)
+//				),
+//
+//				const SizedBox(height: 30),
+//				RaisedButton(
+//					child:Text("Log In"),
+//					color: colors.teal,
+//					onPressed: (){
+//					}
+//				),
+//				Padding(
+//					padding: EdgeInsets.only(top: 45)
+//				),
+//				GestureDetector (
+//					child: Row(
+//						mainAxisAlignment: MainAxisAlignment.center,
 //						children: <Widget>[
 //							Text(
-//								"Email"
-//							),
-//							TextField(
-//								controller: emailLogin,
-//							),
-//							Padding(
-//								padding: EdgeInsets.only(bottom: 15)
+//								"Don't have an account yet? ",
+//								textAlign: TextAlign.center,
+//								style: TextStyle(
+//									fontSize: 14,
+////									color: colors.teal,
+//								),
 //							),
 //							Text(
-//								"Password"
-//							),
-//							TextField(
-//								controller: passwordLogin,
-//							),
-//						],
-//					)
-//				),
-				Padding(
-					padding: EdgeInsets.only(bottom: 15)
-				),
-
-				const SizedBox(height: 30),
-				RaisedButton(
-					child:Text("Log In"),
-					color: colors.teal,
-					onPressed: (){
-					}
-				),
-				Padding(
-					padding: EdgeInsets.only(top: 45)
-				),
-				GestureDetector (
-					child: Row(
-						mainAxisAlignment: MainAxisAlignment.center,
-						children: <Widget>[
-							Text(
-								"Don't have an account yet? ",
-								textAlign: TextAlign.center,
-								style: TextStyle(
-									fontSize: 14,
+//								"Sign-up",
+//								textAlign: TextAlign.center,
+//								style: TextStyle(
+//									fontSize: 14,
 //									color: colors.teal,
-								),
-							),
-							Text(
-								"Sign-up",
-								textAlign: TextAlign.center,
-								style: TextStyle(
-									fontSize: 14,
-									color: colors.teal,
-								),
-							)
-						],
-					),
-					onTap: () {
-					Navigator.push(
-						context,
-						new MaterialPageRoute(builder: (context) => new SignUpPage()),
-					);
-				}
-				),
-			],
-		)
-  );
+//								),
+//							)
+//						],
+//					),
+//					onTap: () {
+//					Navigator.push(
+//						context,
+//						new MaterialPageRoute(builder: (context) => new SignUpPage()),
+//					);
+//				}
+//				),
+//			],
+//		)
+//  );
 
-//  Future<void> signIn() async {
-//  	final formState = _formKey.currentState;
-//  	if(formState.validate()){
-//  		formState.save();
-//  		try {
-//				FirebaseUser user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
-//				Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-//			}catch(e){
-//  			print(e.message);
-//			}
-//		}
-//	}
+  Future<void> signIn() async {
+  	final formState = _formKey.currentState;
+  	if(formState.validate()){
+  		formState.save();
+  		try {
+				await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+				Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+			}catch(e){
+  			print(e.message);
+			}
+		}
+	}
 }
